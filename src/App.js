8ac3fdc0a59544c485879e13092018af1c2cd082
sykang <sykang@inspace.re.kr>
2020-02-28 15:16:01 +0900

    Node + React 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: null };
+  }
+  componentDidMount() {
+    fetch("api")
+      .then(res => res.json())
+      .then(data => this.setState({ username: data.username }));
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const { username } = this.state;
+    return (
+      <div className="App">
+        {" "}
+        <header className="App-header"> {username ? `Hello ${username}` : "Hello World"} </header>{" "}
+      </div>
+    );
+  }
 }
-
 export default App;
