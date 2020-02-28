44d7d602609bff591af9c876dda1dc930a5d1d86
sykang <sykang@inspace.re.kr>
2020-02-28 16:15:18 +0900

    MySQL 연동

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use("/api", (req, res) => res.json({ username: "soyeon" }));
+
+const fs = require("fs");
+const mysql = require("mysql");
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
+app.get("/api/customers", (req, res) => {
+  connection.query("select * from customer", (err, rows, fields) => {
+    res.send(rows);
+  });
+});
+
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: null };
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+// import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: "20px",
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+  prograss: {
+    margin: "20px"
   }
+});
+
+class App extends Component {
+  state = {
+    customers: "",
+    completed: 0
+  };
+
   componentDidMount() {
-    fetch("api")
-      .then(res => res.json())
-      .then(data => this.setState({ username: data.username }));
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
   }
 
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
+
   render() {
-    const { username } = this.state;
+    const { classes } = this.props;
     return (
-      <div className="App">
-        {" "}
-        <header className="App-header"> {username ? `Hello ${username}` : "Hello World"} </header>{" "}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Birth</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Job</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers
+              ? this.state.customers.map(c => {
+                  return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />;
+                })
+              : ""}
+            <TableRow>
+              {/* <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.prograss} variant="determinate" value={this.state.completed} />
+              </TableCell> */}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
-export default App;
+export default withStyles(styles)(App);
